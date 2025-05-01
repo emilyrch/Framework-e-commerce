506b4834b8a17b41d530470a42b25e42a34c30cd
Edgar Vitor <edgarvitor.contato@gmail.com>
2025-05-01 12:06:32 -0300

    uptade commit 2

--- apps/store-ropa/src/app/page.tsx
+++ apps/store-ropa/src/app/page.tsx
@@ -1,19 +1,19 @@
 // apps/store-ropa/src/app/page.tsx
 'use client'
 
-import { products } from '@packages/catalog-module/products'
+  import { products } from '@packages/catalog-module/products'
 
 export default function Home() {
   return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Brechó IC</h1>
+    <main className="p-6 bg-blue-500">
+      <h1 className="text-2xl font-bold mb-4">Catálogo de Produtos</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {products.map(product => (
           <div key={product.id} className="border p-4 rounded shadow">
             <img src={product.imageUrl} alt={product.name} className="w-full h-60 object-cover mb-2 rounded" />
             <h2 className="text-lg font-semibold">{product.name}</h2>
-            <p className="text-sm text-gray-600">{product.description}</p>
-            <p className="text-green-600 font-bold mt-2">R$ {product.price.toFixed(2)}</p>
+            <p className="text-sm text-white-600">{product.description}</p>
+            <p className="text-black-600 font-bold mt-2">R$ {product.price.toFixed(2)}</p>
           </div>
         ))}
       </div>
---
+++ packages/catalog-module/products.ts
@@ -0,0 +1,201 @@
+export type Product = {
+    id: string
+    name: string
+    description: string
+    price: number
+    imageUrl: string
+  }
+  
+  export const products: Product[] = [
+    {
+      id: '1',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 89.9,
+      imageUrl: 'https://plus.unsplash.com/premium_photo-1745337553680-c580ab5f932b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8'
+    },
+    {
+      id: '2',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 129.9,
+      imageUrl: 'https://plus.unsplash.com/premium_photo-1675186049366-64a655f8f537?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      id: '3',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 69.9,
+      imageUrl: 'https://images.unsplash.com/photo-1591047139829-d91aecb6caea?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D'
+    },
+    {
+      id: '4',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 79.9,
+      imageUrl: 'https://images.unsplash.com/photo-1556905055-8f358a7a47b2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D'
+    },{
+      id: '5',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 89.9,
+      imageUrl: 'https://plus.unsplash.com/premium_photo-1745337553680-c580ab5f932b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8'
+    },
+    {
+      id: '6',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 129.9,
+      imageUrl: 'https://plus.unsplash.com/premium_photo-1675186049366-64a655f8f537?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      id: '7',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 69.9,
+      imageUrl: 'https://images.unsplash.com/photo-1591047139829-d91aecb6caea?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D'
+    },
+    {
+      id: '8',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 79.9,
+      imageUrl: 'https://images.unsplash.com/photo-1556905055-8f358a7a47b2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D'
+    },{
+      id: '9',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 89.9,
+      imageUrl: 'https://plus.unsplash.com/premium_photo-1745337553680-c580ab5f932b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8'
+    },
+    {
+      id: '10',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 129.9,
+      imageUrl: 'https://plus.unsplash.com/premium_photo-1675186049366-64a655f8f537?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      id: '11',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 69.9,
+      imageUrl: 'https://images.unsplash.com/photo-1591047139829-d91aecb6caea?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D'
+    },
+    {
+      id: '12',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 79.9,
+      imageUrl: 'https://images.unsplash.com/photo-1556905055-8f358a7a47b2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D'
+    },{
+      id: '13',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 89.9,
+      imageUrl: 'https://plus.unsplash.com/premium_photo-1745337553680-c580ab5f932b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8'
+    },
+    {
+      id: '14',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 129.9,
+      imageUrl: 'https://plus.unsplash.com/premium_photo-1675186049366-64a655f8f537?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      id: '15',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 69.9,
+      imageUrl: 'https://images.unsplash.com/photo-1591047139829-d91aecb6caea?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D'
+    },
+    {
+      id: '16',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 79.9,
+      imageUrl: 'https://images.unsplash.com/photo-1556905055-8f358a7a47b2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D'
+    },{
+      id: '17',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 89.9,
+      imageUrl: 'https://plus.unsplash.com/premium_photo-1745337553680-c580ab5f932b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8'
+    },
+    {
+      id: '18',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 129.9,
+      imageUrl: 'https://plus.unsplash.com/premium_photo-1675186049366-64a655f8f537?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      id: '19',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 69.9,
+      imageUrl: 'https://images.unsplash.com/photo-1591047139829-d91aecb6caea?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D'
+    },
+    {
+      id: '20',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 79.9,
+      imageUrl: 'https://images.unsplash.com/photo-1556905055-8f358a7a47b2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D'
+    },{
+      id: '21',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 89.9,
+      imageUrl: 'https://plus.unsplash.com/premium_photo-1745337553680-c580ab5f932b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8'
+    },
+    {
+      id: '22',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 129.9,
+      imageUrl: 'https://plus.unsplash.com/premium_photo-1675186049366-64a655f8f537?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      id: '23',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 69.9,
+      imageUrl: 'https://images.unsplash.com/photo-1591047139829-d91aecb6caea?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D'
+    },
+    {
+      id: '24',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 79.9,
+      imageUrl: 'https://images.unsplash.com/photo-1556905055-8f358a7a47b2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D'
+    },{
+      id: '25',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 89.9,
+      imageUrl: 'https://plus.unsplash.com/premium_photo-1745337553680-c580ab5f932b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8'
+    },
+    {
+      id: '26',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 129.9,
+      imageUrl: 'https://plus.unsplash.com/premium_photo-1675186049366-64a655f8f537?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D'
+    },
+    {
+      id: '27',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 69.9,
+      imageUrl: 'https://images.unsplash.com/photo-1591047139829-d91aecb6caea?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D'
+    },
+    {
+      id: '28',
+      name: 'Your Product',
+      description: 'Description Description Description',
+      price: 79.9,
+      imageUrl: 'https://images.unsplash.com/photo-1556905055-8f358a7a47b2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D'
+    }
+  ]
+  
